123c6b008af8a8998defbe217e819ff3479fe68d
andy <445998251@qq.com>
2023-12-19 16:17:27 +0800

    Done.

--- src/component/DashboardCard.tsx
+++ src/component/DashboardCard.tsx
@@ -1,49 +1,167 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { Task } from "../page/DashBoard";
+import { collection, doc, query, onSnapshot, where } from "firebase/firestore";
+import { firestore } from "../lib/firebase";
 
-const DashboardItem = styled.div`
-  flex: 1 1 25%;
-  align-self: stretch;
-`;
+// const CardContainer = styled.div`
+//   flex: 1;
+//   display: flex;
+//   flex-direction: column;
+//   border: 1px solid;
+//   border-radius: 1rem;
+//   border-color: #e5e7eb;
+//   margin: 1rem;
+// `;
+
+// const CardItem = styled.div`
+//   flex: 1;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+// `;
+// const CardItemTeam = styled(CardItem)`
+//   flex: 0;
+// `;
 const CardContainer = styled.div`
+  flex: 1;
   display: flex;
   flex-direction: column;
-  border: 1px solid black;
+  color: #333;
+  border: 3px solid #00b4eb;
+  border-radius: 15px;
+  background-color: #ffffff;
   margin: 1rem;
-  height: 100%;
+  // padding: 1.5rem;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
 `;
 
 const CardItem = styled.div`
-  padding: 1rem;
+  flex: 0 0 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  border-bottom: 2px solid #00b4eb;
 `;
 
+const CardItemTeam = styled(CardItem)`
+  padding: 0;
+  & h1 {
+    color: #00b4eb;
+    padding: 0.4em;
+  }
+`;
+const CardItemNumber = styled(CardItem)`
+  padding: 0.5em;
+`;
+const CardItemTask = styled(CardItem)`
+  border-bottom: none;
+  flex: 1;
+  align-items: stretch;
+  justify-content: flex-start;
+`;
+// a single task
+const SubTask = styled.div`
+  font-size: 1.5em;
+  background-color: #00b4eb;
+  border-radius: 0.5em;
+  padding: 5px 0.5em;
+  color: #ffffff;
+  margin: 0.5em;
+`;
+
+type Task = {
+  title: string;
+  rewardFiatAmount: number;
+};
+
 const DashboardCard: React.FC<{
-  name: string;
-  balance: number;
-  tasks: Task[];
-}> = ({ name, balance, tasks }) => {
-  const pendingBalance = tasks.reduce((acc, cur) => {
-    return cur.rewardFiatAmount + acc;
-  }, 0);
-  const totalBalance = balance + pendingBalance;
+  fid: string;
+  uid: string;
+}> = ({ fid, uid }) => {
+  const [name, setName] = useState<string>("");
+  const [balance, setBalance] = useState<number>(0);
+  const [pendingBalance, setPendingBalance] = useState<number>(0);
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const getBalanceByUid = (fid: string, uid: string) => {
+    const docRef = doc(firestore, "Families", fid, "Accounts", uid);
+
+    onSnapshot(docRef, (doc) => {
+      if (!doc.exists()) throw new Error("No such document!");
+      console.log("Current fiatBalance", doc.data().fiatBalance);
+      setBalance(doc.data().fiatBalance);
+    });
+  };
+
+  const getNameByUid = (uid: string) => {
+    const docRef = doc(firestore, "Users", uid, "Restricted", "Profile");
+
+    onSnapshot(docRef, (doc) => {
+      if (!doc.exists()) throw new Error("No such document!");
+      console.log("Current name", doc.data().displayName);
+      setName(doc.data().displayName);
+    });
+  };
+
+  const getOngoingTasksByUid = (fid: string, uid: string) => {
+    const q = query(
+      collection(firestore, "Families", fid, "Tasks"),
+      where("assigneeUIDs", "array-contains", uid), // replace "uid" with the actual uid
+      where("status", "==", "ongoing"),
+      where("rewardFiat", "==", true)
+    );
+
+    onSnapshot(q, (querySnapshot) => {
+      const tasks: Task[] = [];
+      querySnapshot.forEach((doc) => {
+        tasks.push({
+          title: doc.data().title,
+          rewardFiatAmount: doc.data().rewardFiatAmount,
+        });
+      });
+      console.log("Current tasks:", tasks);
+      setTasks(tasks);
+    });
+  };
+  useEffect(() => {
+    getBalanceByUid(fid, uid);
+    getNameByUid(uid);
+    getOngoingTasksByUid(fid, uid);
+  }, [fid, uid]);
+  useEffect(() => {
+    const pendingBalance = tasks.reduce((acc, cur) => {
+      return cur.rewardFiatAmount + acc;
+    }, 0);
+    setPendingBalance(pendingBalance);
+  }, [tasks]);
 
   return (
-    <DashboardItem>
-      <CardContainer>
-        <CardItem>{name}</CardItem>
-        <CardItem>Avaiable Balance: {balance}</CardItem>
-        <CardItem>
-          {tasks.map((task, index) => (
-            <div key={index}>
-              {task.title}: {task.rewardFiatAmount}
-            </div>
-          ))}
-          Pending Balance: {pendingBalance}
-        </CardItem>
-        <CardItem>Total Balance: {totalBalance}</CardItem>
-      </CardContainer>
-    </DashboardItem>
+    <CardContainer>
+      <CardItemTeam>
+        <h1>{name}</h1>
+      </CardItemTeam>
+      <CardItemNumber>
+        <h2>Total</h2>
+        <h1>$ {balance + pendingBalance}</h1>
+      </CardItemNumber>
+      <CardItemNumber>
+        <h2>Cash</h2>
+        <h1>$ {balance}</h1>
+      </CardItemNumber>
+      <CardItemNumber>
+        <h2>Pending Balance</h2>
+        <h1>$ {pendingBalance}</h1>
+      </CardItemNumber>
+      <CardItemTask>
+        {tasks.map((task, index) => (
+          <SubTask key={index}>
+            {task.title} $ {task.rewardFiatAmount}
+          </SubTask>
+        ))}
+      </CardItemTask>
+    </CardContainer>
   );
 };
 
--- src/page/DashBoard.tsx
+++ src/page/DashBoard.tsx
@@ -1,36 +1,22 @@
-import {
-  collection,
-  doc,
-  getDoc,
-  getDocs,
-  query,
-  where,
-} from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
 import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
+import DashboardCard from "../component/DashboardCard";
 import GoogleSignIn from "../component/GoogleSignIn";
 import { firestore } from "../lib/firebase";
 import { useAuth } from "../provider/AuthProvider";
-import DashboardCard from "../component/DashboardCard";
 
 const DashboardContainer = styled.div`
   display: flex;
-  flex-wrap: wrap;
-  margin: 0 auto;
-  justify-content: center;
+  justify-content: space-between;
+  align-items: stretch;
+  height: 100%;
 `;
-
-type AccountDetail = {
-  name: string;
-  balance: number;
-  tasks: Task[];
-};
 
 const DashBoard = () => {
   const { currentUser, logOut } = useAuth();
   const fidRef = useRef<string>(null!);
-  const childAccountsRef = useRef<string[]>([]);
-  const [accountsDetail, setAccountsDetail] = useState<AccountDetail[]>([]);
+  const [childUIDs, setChildUIDs] = useState<string[]>([]);
   useEffect(() => {
     const fetchData = async () => {
       if (!currentUser) return;
@@ -47,72 +33,24 @@ const DashBoard = () => {
       if (!accountsSnap.exists()) {
         throw new Error("No such document!");
       }
-      childAccountsRef.current = accountsSnap.data().childUsers;
-      const accountsDetailConsturctor: AccountDetail[] = [];
-      for (const uid of childAccountsRef.current) {
-        const promises = [
-          getBalanceByUid(fidRef.current, uid),
-          getNameByUid(uid),
-          getOngoingTasksByUid(fidRef.current, uid),
-        ];
-        const [balance, name, tasks] = await Promise.all(promises);
-        accountsDetailConsturctor.push({ name, balance, tasks });
-        console.log(balance, name, tasks);
-      }
-      setAccountsDetail(accountsDetailConsturctor);
+      setChildUIDs(accountsSnap.data().childUsers);
     };
     fetchData().catch((err) => console.error(err));
   }, [currentUser]);
 
-  return !currentUser ? (
-    <GoogleSignIn />
-  ) : (
+  return (
     <>
       <DashboardContainer>
-        {accountsDetail.map((accountDetail, index) => (
-          <DashboardCard key={index} {...accountDetail} />
-        ))}
+        {!currentUser ? (
+          <GoogleSignIn />
+        ) : (
+          childUIDs.map((childUID, index) => (
+            <DashboardCard key={index} fid={fidRef.current} uid={childUID} />
+          ))
+        )}
       </DashboardContainer>
     </>
   );
 };
 
 export default DashBoard;
-
-const getBalanceByUid = async (fid: string, uid: string) => {
-  const accountSnap = await getDoc(
-    doc(firestore, "Families", fid, "Accounts", uid)
-  );
-  if (!accountSnap.exists()) throw new Error("No such document!");
-  return accountSnap.data().fiatBalance;
-};
-const getNameByUid = async (uid: string) => {
-  const userSnap = await getDoc(
-    doc(firestore, "Users", uid, "Restricted", "Profile")
-  );
-  if (!userSnap.exists()) throw new Error("No such document!");
-  return userSnap.data().displayName;
-};
-
-export type Task = {
-  title: string;
-  rewardFiatAmount: number;
-};
-const getOngoingTasksByUid = async (fid: string, uid: string) => {
-  const q = query(
-    collection(firestore, "Families", fid, "Tasks"),
-    where("assigneeUIDs", "array-contains", uid), // replace "uid" with the actual uid
-    where("status", "==", "ongoing"),
-    where("rewardFiat", "==", true)
-  );
-  const tasks: Task[] = [];
-
-  const querySnapshot = await getDocs(q);
-  querySnapshot.forEach((doc) => {
-    tasks.push({
-      title: doc.data().title,
-      rewardFiatAmount: doc.data().rewardFiatAmount,
-    });
-  });
-  return tasks;
-};
